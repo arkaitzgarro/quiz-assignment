104132a47b2a83c30894e4d17a9a0826dda18f48
victor kgong woei lee <>
2021-03-23 17:12:50 +0100

    Fetch 5 questions instead 3

--- src/api/trivia.js
+++ src/api/trivia.js
@@ -3,7 +3,7 @@ export default {
     return this.$http.get('https://opentdb.com/api_category.php')
   },
   getQuestions (params) {
-    return this.$http.get('https://opentdb.com/api.php?amount=3&encode=base64', { params })
+    return this.$http.get('https://opentdb.com/api.php?amount=5&encode=base64', { params })
   },
   getToken () {
     return this.$http.get('https://opentdb.com/api_token.php?command=request')
